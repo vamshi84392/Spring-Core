5ecdabe59880292a257845ddc79ff9da923d9278
Vamshi Krishna Mathukumilli <mathukumilli@Vamshis-MacBook-Pro.local>
2019-04-16 21:57:21 -0400

    Constructor injection implementation

--- src/main/java/com/samplespring/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/samplespring/service/impl/CustomerServiceImpl.java
@@ -10,6 +10,13 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;// = new HibernateCustomerRepositoryImpl();
 
+    public CustomerServiceImpl() {
+    }
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
