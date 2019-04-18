e2e35b15203cc874cac8dea876ae1613de45193c
Vamshi Krishna Mathukumilli <mathukumilli@Vamshis-MacBook-Pro.local>
2019-04-17 21:21:07 -0400

    Reading Properties xml

--- src/main/java/com/samplespring/repository/impl/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/samplespring/repository/impl/HibernateCustomerRepositoryImpl.java
@@ -8,13 +8,20 @@ import java.util.List;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    private String dbUserName;
+
     @Override
     public List<Customer> getAllCustomer() {
+        System.out.println(this.dbUserName);
         List<Customer> customerList = new ArrayList<>();
         Customer customer = new Customer();
         customer.setfName("Vamshi");
         customer.setlName("Mathukumilli");
         customerList.add(customer);
         return customerList;
+    }
+
+    public void setDbUserName(String dbUserName) {
+        this.dbUserName = dbUserName;
     }
 }
