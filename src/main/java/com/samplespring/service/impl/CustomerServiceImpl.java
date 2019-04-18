6b425506d39a80956e3f384bb7dfe63a1e3d4c6e
Vamshi Krishna Mathukumilli <mathukumilli@Vamshis-MacBook-Pro.local>
2019-04-17 20:12:18 -0400

    Autowired final push

--- src/main/java/com/samplespring/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/samplespring/service/impl/CustomerServiceImpl.java
@@ -2,17 +2,35 @@ package com.samplespring.service.impl;
 
 import com.samplespring.model.Customer;
 import com.samplespring.repository.CustomerRepository;
-import com.samplespring.repository.impl.HibernateCustomerRepositoryImpl;
 import com.samplespring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    /*public CustomerServiceImpl() {
+    }*/
+
+    //    @Autowired //Constructor Injection
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        System.out.println("We are using Constructor Injection");
+        this.customerRepository = customerRepository;
+    }
+
+//    @Autowired //member Autowire annotation
+    private CustomerRepository customerRepository;// = new HibernateCustomerRepositoryImpl();
 
     @Override
     public List<Customer> findAll(){
         return customerRepository.getAllCustomer();
+    }
+
+    @Autowired // Setter Injection Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("We are using Setter Injection");
+        this.customerRepository = customerRepository;
     }
 }
